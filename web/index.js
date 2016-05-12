d81eff8fdc33805613b283e6c5b58f159cfb9185
Myeungcheol <myeungcheo@gmail.com>
2016-05-12 22:08:03 +0900

    dynamic web

--- web/index.js
+++ web/index.js
@@ -7,8 +7,26 @@ app.set("view engine", 'ejs');
 //app.use(express.static(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+var data={count:0};
+
 app.get('/', function(req, res){
-  res.render('index');
+  data.count++;
+  res.render('index', data);
+});
+
+app.get('/reset', function(req, res){
+  data.count=0;
+  res.render('index', data);
+})
+
+app.get('/set/count', function(req, res){
+  if(req.query.count) data.count = req.query.count;
+  res.render('index', data);
+});
+
+app.get('/set/:num', function(req, res){
+  data.count=req.params.num;
+  res.render('index', data);
 });
 
 app.listen(2000, function(){
