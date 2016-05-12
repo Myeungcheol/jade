846362efe42a935b78373e3d18a831ab3a11476b
Myeungcheol <myeungcheo@gmail.com>
2016-05-12 21:44:51 +0900

    frist ejs

--- web/index.js
+++ web/index.js
@@ -1,10 +1,15 @@
 
- var express = require('express');
+var express = require('express');
+var path = require('path');
+var app = express();
 
- var app = express();
+app.set("view engine", 'ejs');
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(__dirname + '/public'));
-//app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req, res){
+  res.render('index');
+});
 
 app.listen(2000, function(){
   console.log('Server On!');
